97b8bca576a42b0b6f8a655cf076ec1226651d3e
Dheeraj Saste <dheeraj@nimapinfotech.com>
2023-05-22 10:06:01 +0530

    JS Calculator

---
+++ JS Basic Calculator/calculate.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+let input = document.querySelector("input");
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+
+      input.value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "Back") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
